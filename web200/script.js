a20e1c3178f4ab6d7bf6e2c05c194d104efff8e0
jazmin <97916000+jazmin431451@users.noreply.github.com>
2024-10-24 14:21:27 -0500

    Add files via upload

---
+++ web200/script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var name = "jazmin";
+    document.getElementById("output").innerHTML = "Hello " + name + "!";
+    console.log("Hello " + name + "!");
+});

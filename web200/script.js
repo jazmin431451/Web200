f3845a8241536b0422e80a4ba5b61365dd89eb43
jazmin <97916000+jazmin431451@users.noreply.github.com>
2024-10-24 14:23:40 -0500

    Delete web200/script.js

--- web200/script.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    var name = "jazmin";
-    document.getElementById("output").innerHTML = "Hello " + name + "!";
-    console.log("Hello " + name + "!");
-});
